d0f430e5245b04188c4cfdd0b330b34f082a0292
Cristina Radulescu <cristina.radulescu.bp@nttdata.ro>
2018-12-19 15:56:47 +0200

    #further progress and restructuring

--- src/components/hotelDetails/HotelDetails.js
+++ src/components/hotelDetails/HotelDetails.js
@@ -4,8 +4,9 @@ import EventPlanner from "../plan/EventPlanner";
 import Search from "../plan/Search";
 import Filter from "../filter/Filter";
 import "./HotelDetails.css";
+import LocationView from "../plan/LocationView";
 
-const hotels = [{"id": 1, "name": "Sun Garden", "description": "Very nice hotel and you will have a pleasant stay"},
+const allHotels = [{"id": 1, "name": "Sun Garden", "description": "Very nice hotel and you will have a pleasant stay"},
     {"id": 2, "name": "Wonderland", "description": "Great hotel! Great stay"}]
 
 
@@ -13,27 +14,30 @@ class HotelDetails extends Component {
 
     state = {
         hotelId: null,
-        hotels: []
+        hotels: [],
+        availableLocations:[],
     }
 
     //https://scotch.io/courses/using-react-router-4/route-params
     //kittyrad91@gmail.com
     componentDidMount() {
         const {match : {params}} = this.props;
+        console.log(params);
         this.state.hotelId = params.hotelId;
         console.log("Hotel Id: " + this.state.hotelId);
-        this.state.hotels = this.filterHotel(this.state.hotelId);
-    }
+        this.filterHotel(this.state.hotelId);
+        console.log("after" + this.state.hotels);
 
-    filterHotel = (id) => {
-        let filteredHotels = hotels.filter((hotel) => {
-            return hotel.id === id;
-        })
-        return filteredHotels;
     }
 
     searchShit = param => {
         console.log('Searching for some shit', param);
+    };
+
+    filterHotel =  param => {
+        this.setState({
+            hotels: allHotels.filter(hotel => hotel.id === param)
+        });
     };
 
     //daca am function trebuie sa ii fac bind ca nu stie sa faca bind singur, daca am arrow merge fara bind
@@ -64,9 +68,9 @@ class HotelDetails extends Component {
                             </div>
                         </div>
 
-                        {/*<div class="hotel__details">*/}
-                            {/*{this.state.hotels[0]}.description;*/}
-                        {/*</div>*/}
+                        <div class="hotel__details">
+                            {this.state.hotels[0]}
+                        </div>
                     </section>
                 </main>
             </div>
