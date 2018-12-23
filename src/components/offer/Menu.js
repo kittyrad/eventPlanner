f6265dfb39f4fb8115f0951be8bce81671e322ea
Cristina <kitty_rad@yahoo.com>
2018-12-23 13:41:29 +0200

    #added menu selection

--- src/components/offer/Menu.js
+++ src/components/offer/Menu.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Navigation from "../navigation/Navigation";
+import "./Menu.css";
 
 class Menu extends React.Component {
+
+    menuTypes = [{"id":"1", "name":"Three Course Meal"},
+        {"id":"2", "name":"Swedish Buffet"},
+        {"id":"3", "name":"No Meal"}];
 
     render() {
      return (
@@ -11,8 +16,13 @@ class Menu extends React.Component {
                  <Navigation/>
              </section>
              <section>
-                <div>
-                    Please select Menu type
+                <div class="menu__container">
+                    <div class="menu__title">
+                       Please select Menu type
+                    </div>
+                 </div>
+                 <div>
+                     Next
                  </div>
              </section>
            </main>
